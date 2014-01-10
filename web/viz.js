c18f9438075c4dfbccc2da509f7c6005f92d77c8
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-01-10 16:53:02 -0600

    dean-style time series plots

--- web/viz.js
+++ web/viz.js
@@ -8,9 +8,16 @@ var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
 var x = d3.time.scale()
     .range([0, width])
     .domain([new Date(2013,0,1), new Date(2013,11,31)]);
+var x_bounds = x.domain();
+var x_tick_values = [];
+d3.range(12).forEach(function (month) {
+    x_tick_values.push(new Date(x_bounds[0].getFullYear(), month, 15));
+});
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient("bottom")
+    .tickValues(x_tick_values)
+    .tickFormat(d3.time.format("%B"))
 
 var y = d3.scale.linear()
     .range([height, 0]);
@@ -19,7 +26,6 @@ var yAxis = d3.svg.axis()
     .orient("left");
 
 // convenience methods for converting events to weeks and weeks to dates
-var x_bounds = x.domain();
 var ms_per_week = 86400 * 1000 * 7;
 function event2week(event) {
     return Math.floor((event.datetime - x_bounds[0]) / ms_per_week);
@@ -29,6 +35,13 @@ function week2datetime(week) {
 	x_bounds[0].getFullYear(), x_bounds[0].getMonth(), 
 	x_bounds[0].getDate(), 0, 0, 0, week * ms_per_week
     ))
+}
+function month2datetime(month) {
+    var datetime = new Date(
+	x_bounds[0].getFullYear(), x_bounds[0].getMonth(), x_bounds[0].getDate()
+    )
+    datetime.setMonth(month)
+    return datetime
 }
 
 d3.csv("data/twitter.csv", add_timeseries("twitter"));
@@ -59,28 +72,46 @@ function add_timeseries(data_type) {
 	    return week_events.values.length;
 	})]);
 	
+	// main svg object
 	var svg = d3.select("#"+data_type)
 	    .append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
 	    .append("g")
 	    .attr("transform", "translate("+margin.left+","+margin.top+")");
+
+	// add some backgrounds to highlight the months
+	svg.selectAll(".month")
+	    .data(d3.range(12)).enter()
+	    .append("rect")
+	    .attr("class", function (d){
+		var cls = "odd"
+		if (d%2===0) {
+		    cls = "even"
+		}
+		return "month " + cls;
+	    })
+	    .attr("x", function (d) {return x(month2datetime(d))})
+	    .attr("width", function (d) {return x(month2datetime(d+1))-x(month2datetime(d))})
+	    .attr("y", 0)
+	    .attr("height", height);
     
+	// axes
 	svg.append("g")
 	    .attr("class", "x axis")
 	    .attr("transform", "translate(0," + height + ")")
 	    .call(xAxis);
-    
-	svg.append("g")
+    	svg.append("g")
 	    .attr("class", "y axis")
 	    .call(yAxis)
 	    .append("text")
 	    .attr("transform", "rotate(-90)")
-	    .attr("y", -30)
+	    .attr("y", 5)
 	    .attr("dy", ".71em")
 	    .style("text-anchor", "end")
 	    .text(data_type)
-    
+
+	// show the counts per week
 	svg.selectAll(".count")
 	    .data(data).enter()
 	    .append("rect")
@@ -89,5 +120,6 @@ function add_timeseries(data_type) {
 	    .attr("width", width/52)
 	    .attr("y", function (d) {return y(d.values.length)})
 	    .attr("height", function (d) {return y(0) - y(d.values.length)})
+
     }
 }
